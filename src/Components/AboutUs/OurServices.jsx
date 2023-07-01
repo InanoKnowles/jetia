c3dd13632a1196a0d8ce9805c906e0e102a37729
Ankita Sawrav <ankita.sawrav@gmail.com>
2023-07-01 19:54:28 +1000

    about us services css

---
+++ src/Components/AboutUs/OurServices.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import classes from "./OurServices.module.css";
+
+const OurServices = () => {
+	return (
+		<div id={classes.list}>
+			<ul>
+				<li>Product Design</li>
+				<li>Product Design</li>
+				<li>Product Design</li>
+			</ul>
+
+			<ul>
+				<li>Product Design</li>
+				<li>Product Design</li>
+				<li>Product Design</li>
+			</ul>
+
+			<ul>
+				<li>Product Design</li>
+				<li>Product Design</li>
+				<li>Product Design</li>
+			</ul>
+
+			<ul>
+				<li>Product Design</li>
+				<li>Product Design</li>
+				<li>Product Design</li>
+			</ul>
+
+			<ul>
+				<li>Product Design</li>
+				<li>Product Design</li>
+				<li>Product Design</li>
+			</ul>
+		</div>
+	);
+};
+
+export default OurServices;
