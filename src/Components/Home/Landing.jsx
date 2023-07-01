a513cf755d4bad317ad8eaf0780718b1eae25012
tinanvo <tinanhivo@gmail.com>
2023-07-01 19:52:57 +1000

    Adding styling to to landing section, created button

--- src/Components/Home/Landing.jsx
+++ src/Components/Home/Landing.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 
+import Button from '../UI/Button'
+
 import classes from './Landing.module.css';
+import '../../App.css'
 
 const LandingPage = () => {
   return (
     <div id={classes.landing}>
-    
       <div className={classes.text}>
-      <p>Located in Sydney</p>
+        <p>Located in Sydney</p>
         <div className={classes.topLine}>
-          <p>GROWTH◯</p>
-          <p>FROMTODAY◯</p>
-          <p>EMPOWER◯</p>
-          <p>UNLEASH◯</p>
-          <p>TOFUTURE◯</p>
+          <p className={classes.bigText}>GROWTH</p>
+          <p className={classes.circle}>◯</p>
+          <p className={`${classes.bigText} blue`}>PASSION</p>
+          <p className={classes.circle}>◯</p>
+          <p className={classes.bigText}>UNLEASH</p>
+          <p className={classes.circle}>◯</p>
+          <p className={`${classes.bigText} armyGreen`}>EMPOWER</p>
+          <p className={classes.circle}>◯</p>
+          <p className={classes.bigText}>AMBITIOUS</p>
         </div>
 
         <div className={classes.bottomLine}>
-          <p>UNLEASH◯</p>
-          <p>GROWTH◯</p>
-          <p>TOFUTURE◯</p>
-          <p>FROMTODAY◯</p>
-          <p>EMPOWER◯</p>
+          <p className={`${classes.bigText} orange`}>UNLEASH</p>
+          <p className={classes.circle}>◯</p>
+          <p className={classes.bigText}>AMBITIOUS</p> 
+          <p className={classes.circle}>◯</p>
+          <p className={`${classes.bigText} pink`}>GROWTH</p> 
+          <p className={classes.circle}>◯</p>
+          <p className={classes.bigText}>PASSION</p> 
+          <p className={classes.circle}>◯</p>
+          <p className={classes.bigText}>EMPOWER</p>
         </div>
       </div>
 
       <p className={classes.summary}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Integer feugiat
-        scelerisque varius morbi enim nunc faucibus a.
+        scelerisque varius.
       </p>
 
-      <button>Learn More</button>
+      <Button>Learn More</Button>
     </div>
   );
 };
