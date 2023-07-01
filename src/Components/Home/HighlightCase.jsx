943b70543f86aee85bd77a600b223eb5da4fd7a5
tinanvo <tinanhivo@gmail.com>
2023-07-01 14:18:35 +1000

    Swiper gallery added to highlight case

--- src/Components/Home/HighlightCase.jsx
+++ src/Components/Home/HighlightCase.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
 import classes from './HighlightCase.module.css';
 
 const HighlightCase = () => {
   return (
     <div id={classes.highlightCase}>
       <h2>Highlight Case</h2>
-      <div className={classes.case}>img + text maybe on hover? - carousel </div>
+      <Swiper className={`mySwiper ${classes.swiper}`}>
+        <SwiperSlide className={classes.case}>
+          img + text maybe on hover? - carousel{' '}
+        </SwiperSlide>
+        <SwiperSlide className={classes.case}>
+          img + text maybe on hover? - carousel{' '}
+        </SwiperSlide>
+        <SwiperSlide className={classes.case}>
+          img + text maybe on hover? - carousel{' '}
+        </SwiperSlide>
+        <SwiperSlide className={classes.case}>
+          img + text maybe on hover? - carousel{' '}
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
