75414b215ff510f5719ccfcaf71dc2d049f19721
tinanvo <tinanhivo@gmail.com>
2023-07-01 19:52:57 +1000

    Adding styling to to landing section, created button

--- src/Components/Home/Enquiry/Enquiry.jsx
+++ src/Components/Home/Enquiry/Enquiry.jsx
@@ -7,13 +7,15 @@ import classes from './Enquiry.module.css';
 
 const Enquiry = () => {
   return (
-    <div id={classes.enquiry}>
-      <h3>Curious how we can help?</h3>
-      <h2>Let's Chat</h2>
+    <div id='letschat'>
+      <div id={classes.enquiry}>
+        <h3>Curious how we can help?</h3>
+        <h2>Let's Chat</h2>
 
-      <div className={classes.content}>
-        <EnquiryForm />
-        <ContactDetails />
+        <div className={classes.content}>
+          <EnquiryForm />
+          <ContactDetails />
+        </div>
       </div>
     </div>
   );
