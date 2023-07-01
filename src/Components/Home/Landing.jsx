13e60129dac0cd2a8e9d9c1db59a493e3a38ecec
tinanvo <tinanhivo@gmail.com>
2023-07-01 20:32:57 +1000

    Linked learn more btn to about us page

--- src/Components/Home/Landing.jsx
+++ src/Components/Home/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import Button from '../UI/Button'
+import { Link } from 'react-router-dom';
 
 import classes from './Landing.module.css';
 import '../../App.css'
@@ -41,7 +41,7 @@ const LandingPage = () => {
         scelerisque varius.
       </p>
 
-      <Button>Learn More</Button>
+      <Link to="/aboutus"><Button>Learn More</Button></Link>
     </div>
   );
 };
